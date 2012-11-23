9cc28d5f912e4ad4402b17c6899d20af72634e25
hkk <hanse19@gmail.com>
2012-11-23 16:42:44 +0100

    gtfo rb

--- config.rb
+++
@@ -1,8 +0,0 @@
-# Compass configuration
-http_path       = "/"
-css_dir         = "public/"
-sass_dir        = "public/"
-images_dir      = "images"
-javascripts_dir = "public/"
-
-output_style    = :compact
